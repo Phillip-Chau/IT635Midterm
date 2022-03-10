dbc75f1f134656fe84adc5116e6247fa794521ce
Phillip-Chau <44251521+Phillip-Chau@users.noreply.github.com>
2022-03-09 21:33:57 -0500

    Add files via upload

--- applicationInfo.py
+++ applicationInfo.py
@@ -8,7 +8,10 @@ application_id = int(input())
 cur.execute("""
     SELECT applications.application_name, applications.business_categorization, applications.business_unit, employees.employee_id, employees.employees_name, employees.programming_lang, servers.server_name, servers.ip_address, servers.tech_stack
     FROM applications, employees, servers
-    WHERE application_id=%s""", (application_id,));
+    WHERE application.application_id=employees.application_id
+        AND employees.employees_id=servers.employee_id
+        AND applications.applicaiton_id=%s
+""", (application_id,));
 for row in cur:
     application_name = row[0]
     business_categorization = row[1]
